9cb17d8a2f807c9a9be2e066b1f28316e32f1277
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-11-22 17:04:06 +0300

    init project

---
+++ src/test/java/my/project/user_note/UserNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.user_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
