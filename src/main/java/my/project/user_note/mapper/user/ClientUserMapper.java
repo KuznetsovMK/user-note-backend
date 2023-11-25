8a9c3c1f3cdbadc5de0fe94fb3a73c32b016288e
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-11-25 18:15:58 +0300

    add implementation auth/login endpoint

--- src/main/java/my/project/user_note/mapper/user/ClientUserMapper.java
+++ src/main/java/my/project/user_note/mapper/user/ClientUserMapper.java
@@ -1,5 +1,6 @@
 package my.project.user_note.mapper.user;
 
+import com.model.ClientUserDto;
 import com.model.CreateClientUserRequestDto;
 import my.project.user_note.entity.user.ClientUser;
 import org.mapstruct.Mapper;
@@ -7,5 +8,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ClientUserMapper {
     ClientUser toEntity(CreateClientUserRequestDto request);
+
+    ClientUserDto toDto(ClientUser clientUser);
 }
 
